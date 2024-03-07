4d765f155ac037316076988c6b4060df8248e164
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-07 03:51:07 -0800

    Pascals triangle algorithm

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Pascals Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function for the pascals triangle
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1] * (i + 1) for i in range(n)]
+
+    for i in range(2, n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+    return triangle
