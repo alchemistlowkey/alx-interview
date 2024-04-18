184496fb46bd7764af5081b3a7588f4ec598e821
alchemistlowkey <alchemistlowkey@gmail.com>
2024-04-17 22:37:24 -0700

    semistandard fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,22 +5,22 @@ const request = util.promisify(require('request'));
 
 const movieId = process.argv[2];
 
-async function fetchMovieCharacters(movieId) {
-    const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+async function fetchMovieCharacters (movieId) {
+  const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-    try {
-        const response = await (await request(movieUrl));
-        const movieData = JSON.parse(response.body);
-        const characters = movieData.characters;
+  try {
+    const response = await (await request(movieUrl));
+    const movieData = JSON.parse(response.body);
+    const characters = movieData.characters;
 
-        for (const characterUrl of characters) {
-            const characterResponse = await (await request(characterUrl));
-            const characterData = JSON.parse(characterResponse.body);
-            console.log(characterData.name);
-        }
-    } catch (error) {
-        console.error('Error:', error.message);
+    for (const characterUrl of characters) {
+      const characterResponse = await (await request(characterUrl));
+      const characterData = JSON.parse(characterResponse.body);
+      console.log(characterData.name);
     }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
 fetchMovieCharacters(movieId);
