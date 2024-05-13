f8a1165dfd49a7914465882538252c53bb342343
alchemistlowkey <alchemistlowkey@gmail.com>
2024-05-13 15:12:30 -0700

    prime_game implemented

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""
+Maria and Ben are playing a game.
+Given a set of consecutive integers starting from 1 up to and including n,
+they take turns choosing a prime number from the set and removing that number
+and its multiples from the set.
+The player that cannot make a move loses the game.
+
+They play x rounds of the game, where n may be different for each round.
+Assuming Maria always goes first and both players play optimally,
+determine who the winner of each game is.
+
+Prototype: def isWinner(x, nums)
+where x is the number of rounds and nums is an array of n
+Return: name of the player that won the most rounds
+If the winner cannot be determined, return None
+You can assume n and x will not be larger than 10000
+You cannot import any packages in this task
+Example:
+
+x = 3, nums = [4, 5, 1]
+First round: 4
+
+Maria picks 2 and removes 2, 4, leaving 1, 3
+Ben picks 3 and removes 3, leaving 1
+Ben wins because there are no prime numbers left for Maria to choose
+Second round: 5
+
+Maria picks 2 and removes 2, 4, leaving 1, 3, 5
+Ben picks 3 and removes 3, leaving 1, 5
+Maria picks 5 and removes 5, leaving 1
+Maria wins because there are no prime numbers left for Ben to choose
+Third round: 1
+
+Ben wins because there are no prime numbers for Maria to choose
+Result: Ben has the most wins
+"""
+
+
+def is_prime(number):
+    """
+    Check if a number is prime.
+
+    Args:
+    num (int): The number to check.
+
+    Returns:
+    bool: True if the number is prime, False otherwise.
+    """
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+def isWinner(x, nums):
+    """
+    Determine the winner of the game.
+
+    Args:
+    x (int): The number of rounds.
+    nums (list of int): An array of n for each round.
+
+    Returns:
+    str or None:
+        The name of the player who won the most rounds.
+        If the winner cannot be determined, returns None.
+    """
+    maria = 0
+    ben = 0
+
+    for n in nums:
+        if n == 1:
+            ben += 1
+            continue
+
+        prime_counter = sum(1 for i in range(2, n + 1) if is_prime(i))
+
+        if prime_counter % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    if maria > ben:
+        return "Maria"
+    elif ben > maria:
+        return "Ben"
+    else:
+        return None
